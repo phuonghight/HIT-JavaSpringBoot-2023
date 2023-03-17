3fcc5f204e67dc604d075a00b5981f57e34122bd
hoangphuong <hoangphuong270703@gmail.com>
2023-03-17 20:46:24 +0700

    view and controller by Thymleaf

---
+++ week-02/src/main/java/com/example/week02/Controllor/TestController.java
@@ -0,0 +1,64 @@
+package com.example.week02.Controllor;
+
+import com.example.week02.Student;
+import com.example.week02.Users;
+import org.apache.catalina.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+public class TestController {
+
+    @RequestMapping
+    public ModelAndView index(Model model) {
+        model.addAttribute("name", "Hoang Phuong");
+        Student s = new Student(1, "Phuong");
+        List<Student> ds = new ArrayList<>();
+        ds.add(s);
+        ds.add(new Student(2, "Tien Anh"));
+        ds.add(new Student(3, "Kien"));
+        model.addAttribute("listStudent", ds);
+
+        ModelAndView view = new ModelAndView("index");
+        view.addObject("listStudent", ds);
+        return view;
+    }
+
+//    @RequestMapping("/about")
+//    public String about(@RequestParam("name") String name, @RequestParam("age") String pass, Model model) {
+//
+//        List<Users> users = new ArrayList<>();
+//        users.add(new Users("phuong", "123"));
+//        users.add(new Users("tienanh", "456"));
+//        users.forEach(user -> {
+//            if(user.getName().equals(name) && user.getPass().equals(pass)) {
+//                model.addAttribute("name", name);
+//                model.addAttribute("age", pass);
+//            }
+//        });
+//
+//        return "about";
+//    }
+    @RequestMapping("/about")
+    public String about(@ModelAttribute Users u, Model model) {
+
+        List<Users> users = new ArrayList<>();
+        users.add(new Users("phuong", "123"));
+        users.add(new Users("tienanh", "456"));
+
+        if(users.contains(u)) {
+            model.addAttribute("user", u);
+            return "about";
+        }
+        else return "ErrorPage";
+
+    }
+
+}
