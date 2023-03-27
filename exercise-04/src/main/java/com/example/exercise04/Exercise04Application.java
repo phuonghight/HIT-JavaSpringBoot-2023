6fea0d8ebd044009406645b4b1ab11b96b5d57f8
hoangphuong <hoangphuong270703@gmail.com>
2023-03-28 00:09:54 +0700

    'create-project'

---
+++ exercise-04/src/main/java/com/example/exercise04/Exercise04Application.java
@@ -0,0 +1,13 @@
+package com.example.exercise04;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Exercise04Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Exercise04Application.class, args);
+    }
+
+}
---
+++ exercise-04/src/test/java/com/example/exercise04/Exercise04ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.exercise04;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Exercise04ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
