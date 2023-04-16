0c1b6ebd808767e7a4445d542a8c4e0a0c0e789a
phuong <hoangphuong270703@gmail.com>
2023-04-16 17:19:44 +0700

    init java spring boot project

---
+++ exercise-06/src/main/java/exercise06/ServletInitializer.java
@@ -0,0 +1,13 @@
+package exercise06;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Exercise06Application.class);
+    }
+
+}
---
+++ exercise-06/src/test/java/exercise06/Exercise06ApplicationTests.java
@@ -0,0 +1,13 @@
+package exercise06;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Exercise06ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
